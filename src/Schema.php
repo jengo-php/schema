f3a1add7537f3d7a74695aff21e39e0092c5cffa
Ian Otieno <ochiengian383@gmail.com>
2026-01-24 11:32:24 +0300

    First commit

---
+++ src/Schema.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jengo\Schema;
+
+class Schema
+{
+    public function getSchemaInfo(): string
+    {
+        return 'This is the schema package for CodeIgniter 4 applications.';
+    }
+}
